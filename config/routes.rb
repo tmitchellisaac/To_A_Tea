563e25539e3db09a6ff2cbacde497f5358dbdc87
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-14 15:43:59 -0600

    Add subscription routes with create,index, and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :subscriptions, only: [:create, :index, :update]
+    end
+  end
+
 end
