a8786f378baf2b8617cfc7a984eaddbc2b95f5e7
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-14 16:59:50 -0600

    Move cancellation spec to spec/subscriptions

---
+++ spec/requests/api/v1/subscriptions/cancellation_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Subscription cancellation" do
+  it "will change status of a customer's subscription to cancelled/0" do
+    customer_1 = Customer.create!(first_name: "Donna", last_name: "Bisson", address: "4444 Yellow Tail Dr", email: "donna@gmail.com")
+    camomille = Tea.create!(name: "Camomille", description: "camomille tea", brew_time: "7 minutes", temperature: "170 degrees")
+
+    subscription_data = {
+        customer_id: customer_1.id,
+        tea_id: camomille.id,
+        frequency: 1,
+        price: 1400,
+        title: "Weekly Camomille"
+      }
+  
+    post "/api/v1/customers/#{customer_1.id}/subscriptions", headers: {"CONTENT_TYPE" => "application/json"}, params: JSON.generate(subscription_data)
+  
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    expect(json_response[:data][:attributes][:status]).to eq("active")
+    subscription_id = json_response[:data][:id].to_i
+    subscription = Subscription.last
+    expect(subscription.status).to eq("active")
+
+    patch "/api/v1/customers/#{customer_1.id}/subscriptions/#{subscription_id}?status=cancelled", headers: {"CONTENT_TYPE" => "application/json"}
+    json_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json_response).to eq({success: "Subscription Successfully Cancelled"})
+
+    subscription = Subscription.find(subscription_id)
+    expect(subscription.status).to eq("cancelled")
+  end
+end
